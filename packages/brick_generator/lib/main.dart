c476357321b8f0adbd9074413381921669d10188
Karlo Verde <karlo.verde.1996@gmail.com>
2023-08-20 23:59:41 -0500

    chore(b-gen): create brick generator

---
+++ packages/brick_generator/lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:brick_generator/src/placeholders.dart';
+import 'package:brick_generator/src/reference_file.dart';
+import 'package:brick_generator/src/shell.dart';
+import 'package:path/path.dart' as path;
+
+final referenceAppPath = path.joinAll([
+  'packages',
+  'reference_app',
+]);
+final referenceAppDir = Directory(referenceAppPath);
+
+final brickTemplatePath = path.joinAll([
+  'packages',
+  'brick',
+  '__brick__',
+  'reference_app',
+]);
+final brickTemplateDir = Directory(brickTemplatePath);
+
+Future<void> main(List<String> args) async {
+  // Remove target directory
+  if (brickTemplateDir.existsSync()) {
+    await Shell.removeDirectory(brickTemplateDir);
+  }
+
+  // Copy reference project
+  await Shell.copyDirectory(
+    source: referenceAppDir,
+    destination: brickTemplateDir,
+  );
+
+  // Parametrize template
+  final fsEntities = brickTemplateDir.listSync(recursive: true);
+  await Future.wait<void>([
+    for (final fsEntity in fsEntities)
+      if (fsEntity is File) fsEntity.parametrize(),
+  ]);
+
+  // Use parametrized project name
+  await Shell.renameDirectory(
+    directory: brickTemplateDir,
+    newName: Placeholder.appPackageName.parameter,
+  );
+}
