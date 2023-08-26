610a94ace1f6a9f2d0dceacdb9ecf71ce3369c61
Karlo Verde <karlo.verde.1996@gmail.com>
2023-08-25 23:16:40 -0500

    chore: sliver examples

---
+++ packages/reference_app/lib/home/presentation/sliver_examples.dart
@@ -0,0 +1,17 @@
+import 'package:altoke_app/home/home.dart';
+import 'package:flutter/widgets.dart';
+
+class SliverExamples extends StatelessWidget {
+  const SliverExamples({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList.list(
+      children: [
+        CounterExampleTile(),
+      ],
+    );
+  }
+}
