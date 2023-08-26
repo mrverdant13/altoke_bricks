10553145ed17aabd380b2ce9708dce02fb77da87
Karlo Verde <karlo.verde.1996@gmail.com>
2023-08-26 00:57:14 -0500

    feat: navigate to proper route

--- packages/reference_app/lib/home/presentation/example_tiles.dart
+++ packages/reference_app/lib/home/presentation/example_tiles.dart
@@ -1,4 +1,6 @@
 import 'package:altoke_app/l10n/l10n.dart';
+import 'package:altoke_app/routing/routing.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 
 class CounterExampleTile extends StatelessWidget {
@@ -12,7 +14,9 @@ class CounterExampleTile extends StatelessWidget {
     return ListTile(
       title: Text(l10n.counterExampleLabel),
       onTap: () {
-        // TODO(mrverdant13): Navigate to counter example.
+        context.navigateTo(
+          CounterRoute(),
+        );
       },
     );
   }
