a348ff883d4b412c01e6646aec4aa366cda4bb02
Karlo Verde <karlo.verde.1996@gmail.com>
2024-01-05 02:33:00 -0500

    refactor(ref): relocate root tasks route

--- packages/reference_app/packages/app/lib/routing/routing.dart
+++ packages/reference_app/packages/app/lib/routing/routing.dart
@@ -4,4 +4,4 @@ export 'router.gr.dart';
 /*{{/use_auto_route_router}}*/
 export 'routes/counter/screen.dart';
 export 'routes/home/screen.dart';
-export 'routes/tasks/screen.dart';
+export 'routes/tasks/__root/screen.dart';
