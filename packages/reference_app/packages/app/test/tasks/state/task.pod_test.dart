0faf638ad3c71f4cebf62857817e5264e66c1cc3
Karlo Verde <karlo.verde.1996@gmail.com>
2024-01-10 23:48:09 -0500

    feat(ref): add tests

--- packages/reference_app/packages/app/test/tasks/state/task.pod_test.dart
+++ packages/reference_app/packages/app/test/tasks/state/task.pod_test.dart
@@ -1,10 +1,31 @@
+import 'package:altoke_app/tasks/tasks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test(
-    'task.pod ...',
+    '''
+
+GIVEN a scoped tasks pod
+WHEN the pod is invoked
+THEN the pod should throw an error
+
+''',
     () async {
-      // TODO: Implement test
+      final container = ProviderContainer();
+
+      void action() => container.read(taskPod);
+
+      expect(
+        action,
+        throwsA(
+          isA<UnimplementedError>().having(
+            (error) => error.message,
+            'message',
+            '`taskPod` was not initialized.',
+          ),
+        ),
+      );
     },
   );
 }
