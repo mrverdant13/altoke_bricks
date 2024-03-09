3649818c5032f2921b88bd968dc1379a5bef67cb
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-03-08 21:38:05 -0500

    feat(ref): barrel file for failures (#126)

---
+++ packages/reference_app/packages/tasks_storage/tasks_storage/lib/src/failures/failures.dart
@@ -0,0 +1,2 @@
+export 'create_task.failure.dart';
+export 'update_task.failure.dart';
--- packages/reference_app/packages/tasks_storage/tasks_storage/lib/tasks_storage.dart
+++ packages/reference_app/packages/tasks_storage/tasks_storage/lib/tasks_storage.dart
@@ -1,8 +1,5 @@
 /// An interface for a storage system for tasks.
 library tasks_storage;
 
-export 'src/failures/create_task.failure.dart'
-    show CreateTaskFailure, CreateTaskFailureEmptyTitle;
-export 'src/failures/update_task.failure.dart'
-    show UpdateTaskFailure, UpdateTaskFailureEmptyTitle;
-export 'src/tasks_storage.dart' show TasksStorage;
+export 'src/failures/failures.dart';
+export 'src/tasks_storage.dart';
