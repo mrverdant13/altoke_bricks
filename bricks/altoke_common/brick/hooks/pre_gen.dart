c5ce1c64d6bdc99b9fcbd4c8ac333d88e2a3ea91
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-03-23 17:45:44 -0500

    feat(altoke-common): manual overrides as value equality approach (#154)

--- bricks/altoke_common/brick/hooks/pre_gen.dart
+++ bricks/altoke_common/brick/hooks/pre_gen.dart
@@ -6,6 +6,8 @@ void run(HookContext context) {
   context.vars = {
     ...context.vars,
     ...ValueEqualityApproach.getSelectionMap(context),
+    'use_code_generation':
+        ValueEqualityApproach.getSelectedApproach(context).usesCodeGeneration,
     'using_hooks': true,
   };
 }
