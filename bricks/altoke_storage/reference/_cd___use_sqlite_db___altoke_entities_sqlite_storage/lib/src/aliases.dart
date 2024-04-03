92c4afd3a40c72b4911221c4614e1acb73ab23f2
Karlo Verde <karlo.verde.1996@gmail.com>
2024-04-02 22:19:09 -0500

    feat(altoke-storage): SQLite-based implementation reference

---
+++ bricks/altoke_storage/reference/_cd___use_sqlite_db___altoke_entities_sqlite_storage/lib/src/aliases.dart
@@ -0,0 +1,7 @@
+import 'package:altoke_entities_sqlite_storage/altoke_entities.drift.dart';
+
+/// A SQLite companion class for Altoke Entities.
+typedef DriftAltokeEntitiesCompanion = AltokeEntitiesCompanion;
+
+/// A SQLite DAO for Altoke Entities.
+typedef DriftAltokeEntitiesDao = AltokeEntitiesDrift;
