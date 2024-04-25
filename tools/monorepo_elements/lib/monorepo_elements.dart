6c35d0f74219f55836451f9f4ee5444b5f7a5c18
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-04-25 00:35:58 -0500

    refactor(t-monorepo-env): create new containing package (#278)
    refactor(t-altoke-monorepo-env): create new containing package

---
+++ tools/monorepo_elements/lib/monorepo_elements.dart
@@ -0,0 +1,8 @@
+/// A set of elements related to the Altoke Bricks monorepo.
+library monorepo_elements;
+
+export 'src/brick_generator.dart';
+export 'src/dirs.dart';
+export 'src/exceptions/exceptions.dart';
+export 'src/files.dart';
+export 'src/vars.dart';
---
+++ tools/monorepo_elements/lib/src/exceptions/invalid_brick_hooks_directory.dart
@@ -0,0 +1,36 @@
+/// {@template invalid_brick_hooks_dir_error}
+/// Error thrown when the brick hooks directory is invalid.
+/// {@endtemplate}
+sealed class InvalidBrickHooksDirError implements Exception {
+  const InvalidBrickHooksDirError(
+    this.reason,
+  );
+
+  /// Reason for the error.
+  final String reason;
+
+  @override
+  String toString() {
+    return 'Invalid brick hooks directory.\n$reason';
+  }
+}
+
+/// {@template invalid_brick_hooks_name_error}
+/// Error thrown when the brick hooks name is invalid.
+/// {@endtemplate}
+class InvalidBrickHooksNameError extends InvalidBrickHooksDirError {
+  /// {@macro invalid_brick_hooks_name_error}
+  const InvalidBrickHooksNameError({
+    required String description,
+  }) : super(description);
+}
+
+/// {@template brick_hooks_dir_not_found_error}
+/// Error thrown when the brick hooks directory is not found.
+/// {@endtemplate}
+class BrickHooksDirNotFoundError extends InvalidBrickHooksDirError {
+  /// {@macro brick_hooks_dir_not_found_error}
+  const BrickHooksDirNotFoundError({
+    required String hooksPath,
+  }) : super('Brick hooks directory not found ($hooksPath).');
+}
