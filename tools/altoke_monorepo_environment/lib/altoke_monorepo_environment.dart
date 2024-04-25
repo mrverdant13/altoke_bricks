b6b541392b4896f5815ad08d25b132e90388fce2
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-04-25 00:44:37 -0500

    chore: drop legacy monorepo env (#281)

--- tools/altoke_monorepo_environment/lib/altoke_monorepo_environment.dart
+++
@@ -1,8 +0,0 @@
-/// A set of instances and utilities related to an Altoke Bricks monorepo.
-library altoke_monorepo_environment;
-
-export 'src/brick_generator.dart';
-export 'src/dirs.dart';
-export 'src/exceptions/exceptions.dart';
-export 'src/files.dart';
-export 'src/vars.dart';
--- tools/altoke_monorepo_environment/lib/src/exceptions/exceptions.dart
+++
@@ -1,6 +0,0 @@
-export 'invalid_brick_directory.dart';
-export 'invalid_brick_hooks_directory.dart';
-export 'invalid_brick_scope_directory.dart';
-export 'invalid_package_directory.dart';
-export 'invalid_root_directory.dart';
-export 'invalid_runtime.dart';
