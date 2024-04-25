fe1b1e0ac3b8971412140595eab94272aae1cce8
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-04-24 22:16:14 -0500

    feat(t-altoke-monorepo-env): brick mason generators (#275)

--- tools/altoke_monorepo_environment/lib/altoke_monorepo_environment.dart
+++ tools/altoke_monorepo_environment/lib/altoke_monorepo_environment.dart
@@ -1,6 +1,7 @@
 /// A set of instances and utilities related to an Altoke Bricks monorepo.
 library altoke_monorepo_environment;
 
+export 'src/brick_generator.dart';
 export 'src/dirs.dart';
 export 'src/exceptions/exceptions.dart';
 export 'src/files.dart';
