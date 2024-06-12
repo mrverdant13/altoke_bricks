624d4256ff5d1e48b9d49e984b8978ff6223a7ca
Karlo Verde <karlo.verde.1996@gmail.com>
2024-06-12 01:07:50 -0500

    feat(b-altoke-dart-pkg): create reference

---
+++ bricks/altoke_dart_package/reference/lib/src/altoke_dart_package.dart
@@ -0,0 +1,7 @@
+/// {@template altoke_dart_package.altoke_dart_package}
+/// # Altoke Dart Package
+/// {@endtemplate}
+class AltokeDartPackage {
+  /// {@macro altoke_dart_package.altoke_dart_package}
+  const AltokeDartPackage();
+}
