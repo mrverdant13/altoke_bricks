62801c371f7ac323ea651d24a194d301f1723412
Karlo Verde <42245236+mrverdant13@users.noreply.github.com>
2024-06-23 00:57:20 -0500

    feat(t-shell): add new utilities (#350)

--- tools/shell/lib/shell.dart
+++ tools/shell/lib/shell.dart
@@ -1,130 +1,67 @@
-import 'dart:io';
+import 'dart:async';
+import 'dart:io' hide stderr, stdin, stdout;
 
 /// Shell utilities.
-class Shell {
-  /// Removes untracked files from the provided [directory].
-  static Future<void> gitCleanDirectory(
-    Directory directory,
-  ) async {
-    await run('git clean -dfX ${directory.path}');
-  }
-
+abstract class Shell {
   /// Copies a directory located at [source] to [destination].
   static Future<void> copyDirectory({
     required Directory source,
     required Directory destination,
+    Stdin? stdin,
+    Stdout? stdout,
+    Stdout? stderr,
+    AsyncVoidCallback? onStart,
+    AsyncVoidCallback? onSuccess,
+    AsyncVoidHandlerCallback<ExceptionDetails>? onError,
   }) {
-    if (Platform.isLinux || Platform.isMacOS) {
-      return run('cp -r ${source.path} ${destination.path}');
-    }
-    if (Platform.isWindows) {
-      // cspell: disable-next-line
-      return run('cmd /c xcopy ${source.path} ${destination.path} /E /I /Y');
-    }
-    throw UnsupportedError('Unsupported platform');
+    final fullCommand = () {
+      if (Platform.isLinux || Platform.isMacOS) {
+        return 'cp -r ${source.path} ${destination.path}';
+      }
+      if (Platform.isWindows) {
+        // cspell: disable-next-line
+        return 'cmd /c xcopy ${source.path} ${destination.path} /E /I /Y';
+      }
+    }();
+    if (fullCommand == null) throw UnsupportedError('Unsupported platform');
+    return run(
+      fullCommand,
+      stdin: stdin,
+      stdout: stdout,
+      stderr: stderr,
+      onStart: onStart,
+      onSuccess: onSuccess,
+      onError: onError,
+    );
   }
 
   /// Removes a [directory].
   static Future<void> removeDirectory(
-    Directory directory,
-  ) {
-    if (Platform.isLinux || Platform.isMacOS) {
-      return run('rm -rf ${directory.path}');
-    }
-    if (Platform.isWindows) {
-      return run('cmd /c rmdir ${directory.path} /S /Q');
-    }
-    throw UnsupportedError('Unsupported platform');
-  }
-
-  /// Formats Dart files in the provided [directory].
-  static Future<void> dartFormat(
     Directory directory, {
-    bool failOnChanges = false,
-  }) async {
-    await run(
-      [
-        'dart format',
-        if (failOnChanges) '--set-exit-if-changed',
-        '.',
-      ].join(' '),
-      workingDir: directory.path,
-    );
-  }
-
-  /// Analyzes Dart files in the provided [directory].
-  static Future<void> dartAnalyze(
-    Directory directory, {
-    bool fatalInfos = false,
-    bool fatalWarnings = false,
-  }) async {
-    await run(
-      [
-        'dart analyze',
-        if (fatalInfos) '--fatal-infos',
-        if (fatalWarnings) '--fatal-warnings',
-        '.',
-      ].join(' '),
-      workingDir: directory.path,
-    );
-  }
-
-  /// Runs tests in the provided [directory].
-  static Future<void> dartTest(
-    Directory directory, {
-    Directory? coverageDirectory,
-  }) async {
-    await run(
-      [
-        'dart test',
-        if (coverageDirectory != null) '--coverage=${coverageDirectory.path}',
-        '-r expanded',
-        '--test-randomize-ordering-seed random',
-      ].join(' '),
-      workingDir: directory.path,
-    );
-  }
-
-  /// Formats coverage data.
-  static Future<void> formatCoverageAsLcov({
-    required Directory input,
-    required File output,
-    required Directory reportOn,
-  }) async {
-    await run(
-      [
-        'format_coverage',
-        '--lcov',
-        '--in=${input.path}',
-        '--out=${output.path}',
-        '--report-on=${reportOn.path}',
-      ].join(' '),
-    );
-  }
-
-  /// Filters coverage data.
-  static Future<void> filterCoverage({
-    required File inputLcov,
-    required File outputLcov,
-    required List<String> filters,
-  }) async {
-    await run(
-      [
-        'coverde filter',
-        '--input ${inputLcov.path}',
-        '--output ${outputLcov.path}',
-        '--filters ${filters.join(',')}',
-      ].join(' '),
-    );
-  }
-
-  /// Checks coverage.
-  static Future<void> checkCoverage({
-    required File inputLcov,
-    required int threshold,
-  }) async {
-    await run(
-      'coverde check -i ${inputLcov.path} $threshold',
+    Stdin? stdin,
+    Stdout? stdout,
+    Stdout? stderr,
+    AsyncVoidCallback? onStart,
+    AsyncVoidCallback? onSuccess,
+    AsyncVoidHandlerCallback<ExceptionDetails>? onError,
+  }) {
+    final fullCommand = () {
+      if (Platform.isLinux || Platform.isMacOS) {
+        return 'rm -rf ${directory.path}';
+      }
+      if (Platform.isWindows) {
+        return 'cmd /c rmdir ${directory.path} /S /Q';
+      }
+    }();
+    if (fullCommand == null) throw UnsupportedError('Unsupported platform');
+    return run(
+      fullCommand,
+      stdin: stdin,
+      stdout: stdout,
+      stderr: stderr,
+      onStart: onStart,
+      onSuccess: onSuccess,
+      onError: onError,
     );
   }
 
@@ -133,6 +70,11 @@ class Shell {
     String fullCommand, {
     String? workingDir,
     Stdin? stdin,
+    Stdout? stdout,
+    Stdout? stderr,
+    AsyncVoidCallback? onStart,
+    AsyncVoidCallback? onSuccess,
+    AsyncVoidHandlerCallback<ExceptionDetails>? onError,
   }) async {
     final [executable, ...arguments] = fullCommand.split(' ');
     final process = await Process.start(
@@ -141,26 +83,58 @@ class Shell {
       workingDirectory: workingDir,
       runInShell: true,
     );
-    final outBuf = StringBuffer()..writeln();
-    final (_, _, _, exitCode) = await (
-      process.stdout.forEach((raw) {
-        final record = String.fromCharCodes(raw);
-        outBuf.write('\x1B[34m$record\x1B[0m');
-      }),
-      process.stderr.forEach((raw) {
-        final record = String.fromCharCodes(raw);
-        outBuf.write('\x1B[31m$record\x1B[0m');
-      }),
-      stdin != null ? process.stdin.addStream(stdin) : Future<void>.value(),
-      process.exitCode,
-    ).wait;
-    if (exitCode != 0) {
-      throw ProcessException(
-        '\x1B[31m$executable\x1B[0m',
-        [for (final argument in arguments) '\x1B[31m$argument\x1B[0m'],
-        outBuf.toString(),
-        exitCode,
-      );
+    await onStart?.call();
+    try {
+      final outBuf = StringBuffer()..writeln();
+      final [int exitCode, ...] = await [
+        process.exitCode,
+        process.stdout.forEach((raw) {
+          final record = String.fromCharCodes(raw);
+          outBuf.write('\x1B[34m$record\x1B[0m');
+        }),
+        process.stderr.forEach((raw) {
+          final record = String.fromCharCodes(raw);
+          outBuf.write('\x1B[31m$record\x1B[0m');
+        }),
+        if (stdin != null) process.stdin.addStream(stdin),
+        if (stdout != null) stdout.addStream(process.stdout),
+        if (stderr != null) stderr.addStream(process.stderr),
+      ].wait;
+      if (exitCode != 0) {
+        throw ProcessException(
+          '\x1B[31m$executable\x1B[0m',
+          [for (final argument in arguments) '\x1B[31m$argument\x1B[0m'],
+          outBuf.toString(),
+          exitCode,
+        );
+      }
+      await onSuccess?.call();
+    } catch (error, stackTrace) {
+      await onError?.call(ExceptionDetails(error, stackTrace));
+      rethrow;
     }
   }
+}
+
+/// An async callback that does not return a value.
+typedef AsyncVoidCallback = Future<void> Function();
+
+/// An async callback that takes a parameter and does not return a value.
+typedef AsyncVoidHandlerCallback<T> = Future<void> Function(T);
+
+/// {@template shell.exception_details}
+/// # Exception Details
+/// {@endtemplate}
+class ExceptionDetails {
+  /// {@macro shell.exception_details}
+  const ExceptionDetails(
+    this.exception,
+    this.stackTrace,
+  );
+
+  /// The exception.
+  final Object exception;
+
+  /// The stack trace.
+  final StackTrace stackTrace;
 }
