091bae10f6f6f10b5a3d2667acbbac1e559ad005
Karlo Verde <karlo.verde.1996@gmail.com>
2024-10-14 12:06:20 -0500

    feat(t-local-database-alternative): create local DB alternative helper

---
+++ tools/local_database_alternative/lib/local_database_alternative.dart
@@ -0,0 +1,4 @@
+/// Local database alternative.
+library local_database_alternative;
+
+export 'src/local_database_alternative.dart';
---
+++ tools/local_database_alternative/lib/src/local_database_alternative.dart
@@ -0,0 +1,52 @@
+import 'package:mason/mason.dart';
+
+/// Local database alternative.
+enum LocalDatabaseAlternative {
+  /// Drift (SQLite).
+  ///
+  /// SQL database.
+  drift('drift'),
+
+  // TODO(mrverdant13): Support Hive.
+
+  /// Isar.
+  ///
+  /// NoSQL database.
+  isar('isar'),
+
+  // TODO(mrverdant13): Support Sembast.
+  ;
+
+  const LocalDatabaseAlternative(this.varIdentifier);
+
+  factory LocalDatabaseAlternative.fromVarIdentifier(String varIdentifier) {
+    return LocalDatabaseAlternative.values.firstWhere(
+      (alternative) => alternative.varIdentifier == varIdentifier,
+      orElse: () => throw ArgumentError(
+        'Invalid local database alternative: $varIdentifier',
+      ),
+    );
+  }
+
+  /// The variable identifier.
+  final String varIdentifier;
+
+  /// The variable key.
+  static const varKey = 'local_database_alternative';
+
+  /// Returns a [Map] of variables that represent the local database alternative
+  /// selection.
+  static Map<String, bool> getSelectionMap(HookContext context) {
+    final selectedAlternative = getSelectedAlternative(context);
+    return {
+      for (final alternative in LocalDatabaseAlternative.values)
+        'use_${alternative.varIdentifier}': alternative == selectedAlternative,
+    };
+  }
+
+  /// Returns the selected local database alternative.
+  static LocalDatabaseAlternative getSelectedAlternative(HookContext context) {
+    final selectedAlternative = context.vars[varKey]! as String;
+    return LocalDatabaseAlternative.fromVarIdentifier(selectedAlternative);
+  }
+}
