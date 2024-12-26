63fd8458f017a18e38c60198a9f1a1e0f6370ff2
Karlo Verde <karlo.verde.1996@gmail.com>
2024-12-26 18:28:19 -0500

    feat(t-monorepo-elements): add directory utilities

--- tools/monorepo_elements/lib/src/files.dart
+++ tools/monorepo_elements/lib/src/files.dart
@@ -1,64 +1,34 @@
 import 'dart:io';
 
 import 'package:monorepo_elements/monorepo_elements.dart';
-import 'package:path/path.dart' as path;
 
 /// Files related to a monorepo environment.
 abstract final class Files {
   /// Root README file of the monorepo.
   static final rootReadme = () {
-    final file = File(
-      path.join(
-        Vars.rootPath,
-        'README.md',
-      ),
-    );
-    if (!file.existsSync()) {
-      throw RootReadmeNotFoundError(readmePath: file.path);
-    }
-    return file;
+    final file = Dirs.root.descendantFile('README.md');
+    if (file.existsSync()) return file;
+    throw RootReadmeNotFoundError(readmePath: file.path);
   }();
 
   /// Brick generation data file of a brick scope managed by the monorepo.
   static final brickGenData = () {
     final file = File(Vars.scopeBrickGenDataPath);
-    if (!file.existsSync()) {
-      throw BrickScopeGenDataNotFoundError(
-        brickGenDataPath: file.path,
-      );
-    }
-    return file;
+    if (file.existsSync()) return file;
+    throw BrickScopeGenDataNotFoundError(brickGenDataPath: file.path);
   }();
 
   /// README file of a brick scope managed by the monorepo.
   static final brickReadme = () {
-    final file = File(
-      path.join(
-        Vars.brickPath,
-        'README.md',
-      ),
-    );
-    if (!file.existsSync()) {
-      throw BrickReadmeNotFoundError(
-        readmePath: file.path,
-      );
-    }
-    return file;
+    final file = Dirs.brick.descendantFile('README.md');
+    if (file.existsSync()) return file;
+    throw BrickReadmeNotFoundError(readmePath: file.path);
   }();
 
   /// Manifest file of a brick managed by the monorepo.
   static final brickManifest = () {
-    final file = File(
-      path.join(
-        Vars.brickPath,
-        'brick.yaml',
-      ),
-    );
-    if (!file.existsSync()) {
-      throw BrickManifestNotFoundError(
-        manifestPath: file.path,
-      );
-    }
-    return file;
+    final file = Dirs.brick.descendantFile('brick.yaml');
+    if (file.existsSync()) return file;
+    throw BrickManifestNotFoundError(manifestPath: file.path);
   }();
 }
