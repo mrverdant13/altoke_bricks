2e2044184570b07de3008d3df2c64f48d2c97a46
Karlo Verde <karlo.verde.1996@gmail.com>
2024-12-31 12:55:49 -0500

    feat(b-altoke-app): add Android platform support

--- bricks/altoke_app/brick/hooks/pre_gen.dart
+++ bricks/altoke_app/brick/hooks/pre_gen.dart
@@ -1,11 +1,24 @@
 import 'package:mason/mason.dart';
 
+import 'src/android_application_identifier.dart';
+import 'src/android_identifier_segment.dart';
+import 'src/android_organization.dart';
 import 'src/router.dart';
 
 void run(HookContext context) {
+  final rawAndroidOrganization = context.vars['androidOrganization'] as String;
+  final androidOrganization = AndroidOrganization(rawAndroidOrganization);
+  final projectName = context.vars['projectName'] as String;
+  final androidProjectName = AndroidIdentifierSegment(projectName);
+  final androidApplicationIdentifier = AndroidApplicationIdentifier.fromParts(
+    organization: androidOrganization,
+    name: androidProjectName,
+  );
   context.vars = {
     ...context.vars,
+    'androidApplicationIdentifierAsPath':
+        androidApplicationIdentifier.segments.join('/'),
+    'androidApplicationIdentifier': androidApplicationIdentifier,
     ...RouterPackage.getSelectionMap(context),
-    'using_hooks': true,
   };
 }
