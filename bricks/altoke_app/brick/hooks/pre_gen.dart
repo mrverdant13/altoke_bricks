87e44cbeb02d531fb1ebe758ee92d60e1163e1f2
Karlo Verde <karlo.verde.1996@gmail.com>
2025-01-01 22:57:17 -0500

    refactor(b-altoke-app): use snake case for var keys

--- bricks/altoke_app/brick/hooks/pre_gen.dart
+++ bricks/altoke_app/brick/hooks/pre_gen.dart
@@ -1,14 +1,14 @@
 import 'package:mason/mason.dart';
+import 'package:router_package/router_package.dart';
 
 import 'src/android_application_identifier.dart';
 import 'src/android_identifier_segment.dart';
 import 'src/android_organization.dart';
-import 'src/router.dart';
 
 void run(HookContext context) {
-  final rawAndroidOrganization = context.vars['androidOrganization'] as String;
+  final rawAndroidOrganization = context.vars['android_organization'] as String;
   final androidOrganization = AndroidOrganization(rawAndroidOrganization);
-  final projectName = context.vars['projectName'] as String;
+  final projectName = context.vars['project_name'] as String;
   final androidProjectName = AndroidIdentifierSegment(projectName);
   final androidApplicationIdentifier = AndroidApplicationIdentifier.fromParts(
     organization: androidOrganization,
@@ -16,9 +16,9 @@ void run(HookContext context) {
   );
   context.vars = {
     ...context.vars,
-    'androidApplicationIdentifierAsPath':
+    'android_application_identifier_as_path':
         androidApplicationIdentifier.segments.join('/'),
-    'androidApplicationIdentifier': androidApplicationIdentifier,
-    ...RouterPackage.getSelectionMap(context),
+    'android_application_identifier': androidApplicationIdentifier,
+    ...context.routerPackageSelectionMap,
   };
 }
