26fe1fdefac62fef6d399db2753baf26133c0a0b
Karlo Verde <karlo.verde.1996@gmail.com>
2025-01-02 11:18:09 -0500

    fix(b-altoke-app): use safer vars check

--- bricks/altoke_app/brick/hooks/pre_gen.dart
+++ bricks/altoke_app/brick/hooks/pre_gen.dart
@@ -1,24 +1,29 @@
 import 'package:mason/mason.dart';
-import 'package:router_package/router_package.dart';
 
 import 'src/android_application_identifier.dart';
 import 'src/android_identifier_segment.dart';
 import 'src/android_organization.dart';
 
 void run(HookContext context) {
-  final rawAndroidOrganization = context.vars['android_organization'] as String;
-  final androidOrganization = AndroidOrganization(rawAndroidOrganization);
   final projectName = context.vars['project_name'] as String;
-  final androidProjectName = AndroidIdentifierSegment(projectName);
-  final androidApplicationIdentifier = AndroidApplicationIdentifier.fromParts(
-    organization: androidOrganization,
-    name: androidProjectName,
-  );
-  context.vars = {
-    ...context.vars,
-    'android_application_identifier_as_path':
-        androidApplicationIdentifier.segments.join('/'),
-    'android_application_identifier': androidApplicationIdentifier,
-    ...context.routerPackageSelectionMap,
-  };
+  switch (context.vars['android_organization']) {
+    case null:
+      context.vars.addAll({
+        'include_android_platform': false,
+      });
+    case final String rawAndroidOrganization:
+      final androidOrganization = AndroidOrganization(rawAndroidOrganization);
+      final androidProjectName = AndroidIdentifierSegment(projectName);
+      final androidApplicationIdentifier =
+          AndroidApplicationIdentifier.fromParts(
+        organization: androidOrganization,
+        name: androidProjectName,
+      );
+      context.vars.addAll({
+        'include_android_platform': true,
+        'android_application_identifier_as_path':
+            androidApplicationIdentifier.segments.join('/'),
+        'android_application_identifier': androidApplicationIdentifier,
+      });
+  }
 }
