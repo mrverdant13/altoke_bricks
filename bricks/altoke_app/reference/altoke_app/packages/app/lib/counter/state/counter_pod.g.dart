5714c0f5de504f36c5ef3e507a03e0498ffd3105
Karlo Verde <karlo.verde.1996@gmail.com>
2025-11-11 15:15:30 -0500

    chore(b-app): update generated code

--- bricks/altoke_app/reference/altoke_app/packages/app/lib/counter/state/counter_pod.g.dart
+++ bricks/altoke_app/reference/altoke_app/packages/app/lib/counter/state/counter_pod.g.dart
@@ -6,6 +6,9 @@ part of 'counter_pod.dart';
 // RiverpodGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint, type=warning
+
 @ProviderFor(Counter)
 const counterPod = CounterProvider._();
 
@@ -57,6 +60,3 @@ abstract class _$Counter extends $Notifier<int> {
     element.handleValue(ref, created);
   }
 }
-
-// ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
