561d2b15241bfc3357bfa593a1ffc61571a5163a
Karlo Verde <karlo.verde.1996@gmail.com>
2025-12-09 01:38:48 -0500

    chore(b-app): generate brick template

--- bricks/altoke_app/brick/__brick__/{{#requirements_met}}{{project_name.snakeCase()}}{{/requirements_met}}/packages/app/lib/app/state/state.dart
+++ bricks/altoke_app/brick/__brick__/{{#requirements_met}}{{project_name.snakeCase()}}{{/requirements_met}}/packages/app/lib/app/state/state.dart
@@ -1,16 +1,5 @@
-
-
-/*x{{#use_bloc}}#*/
-export '{{#use_bloc}}app_initialization_bloc.dart{{/use_bloc}}';
-/*x{{/use_bloc}}#x*/
-/*x{{#use_riverpod}}#*/
-export '{{#use_riverpod}}async_initialization_pod.dart{{/use_riverpod}}';
-/*x{{/use_riverpod}}#*/
-/*{{#use_bloc}}#x*/
-export '{{#use_bloc}}bloc_observer.dart{{/use_bloc}}';
-/*x{{/use_bloc}}#x*/
-/*x{{#use_riverpod}}#x*/
-export '{{#use_riverpod}}provider_observer.dart{{/use_riverpod}}';
-
-/*x{{/use_riverpod}}#*/
+{{#use_bloc}}
+export '{{#use_bloc}}app_initialization_bloc.dart{{/use_bloc}}';{{/use_bloc}}{{#use_riverpod}}
+export '{{#use_riverpod}}async_initialization_pod.dart{{/use_riverpod}}';{{/use_riverpod}}
+{{#use_bloc}}export '{{#use_bloc}}bloc_observer.dart{{/use_bloc}}';{{/use_bloc}}{{#use_riverpod}}export '{{#use_riverpod}}provider_observer.dart{{/use_riverpod}}';{{/use_riverpod}}
 
