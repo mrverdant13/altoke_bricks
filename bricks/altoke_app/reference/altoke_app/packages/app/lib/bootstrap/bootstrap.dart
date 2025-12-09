29e8d5c0ade76fc07fa529629b6b0f22308e7f53
Karlo Verde <karlo.verde.1996@gmail.com>
2025-12-09 18:28:32 -0500

    fix(b-app): drop unnecessary lint ignore

--- bricks/altoke_app/reference/altoke_app/packages/app/lib/bootstrap/bootstrap.dart
+++ bricks/altoke_app/reference/altoke_app/packages/app/lib/bootstrap/bootstrap.dart
@@ -79,8 +79,8 @@ Future<void> bootstrap( /*{{#use_riverpod}}*/ {
               return /*remove-end*/ /*{{#use_bloc}}*/ BlocProvider(
                 create: (context) => AppInitializationBloc(
                   // Defining a general purpose TODO
-                  // ignore: flutter_style_todos, lines_longer_than_80_chars
-                  // TODO: Provide initialization callbacks or other dependencies.
+                  // ignore: flutter_style_todos
+                  // TODO: Provide initialization callbacks or dependencies.
                   initializationCallback: () async {},
                   /*remove-start*/
                   applicationDocumentsDirectoryGetter:
