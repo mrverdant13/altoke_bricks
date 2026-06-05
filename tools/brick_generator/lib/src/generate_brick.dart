2ec4cf0b29f5bf9fd5e476769cd586b9be4b4f2a
Karlo Verde <karlo.verde.1996@gmail.com>
2026-06-04 23:02:35 -0500

    build: upgrade testing deps

--- tools/brick_generator/lib/src/generate_brick.dart
+++ tools/brick_generator/lib/src/generate_brick.dart
@@ -40,11 +40,13 @@ ResolvedBrickScope resolveBrickScope({String? scopePath}) {
       brickTemplateDir: Directory(p.join(scopePath, 'brick', '__brick__')),
     );
   }
-  return ResolvedBrickScope( // coverage:ignore-line
-    scopeDir: Dirs.scope, // coverage:ignore-line
-    brickGenDataFile: Files.brickGenData, // coverage:ignore-line
-    brickTemplateDir: Dirs.brickTemplate, // coverage:ignore-line
-  ); // coverage:ignore-line
+  // coverage:ignore-start
+  return ResolvedBrickScope(
+    scopeDir: Dirs.scope,
+    brickGenDataFile: Files.brickGenData,
+    brickTemplateDir: Dirs.brickTemplate,
+  );
+  // coverage:ignore-end
 }
 
 /// Generates a brick template from the current scope's reference project.
